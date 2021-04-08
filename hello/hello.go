4fe767edfe65837c4e4bbc236d6b59340dd92d2d
gavin <gavinyjb@gmail.com>
2021-04-08 22:08:18 +0800

    已验证的hello文件

---
+++ hello/hello.go
@@ -0,0 +1,18 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+//     fmt.Println("Hello, world")
+// }
+package main
+
+import "fmt"
+
+func Hello(name string) string {
+    return "Hello, "+name
+}
+
+func main() {
+    fmt.Println(Hello("world"))
+}
