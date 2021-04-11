2d5cf918e08733a2d0c2980ad5d8c9514d11b17b
gavin <gavinyjb@gmail.com>
2021-04-12 00:20:17 +0800

    语法看着有点儿复杂，没关系，慢慢来。 httptest.NewServer 接受一个我们传入的 匿名函数 http.HandlerFunc。 http.HandlerFunc 是一个看起来类似这样的类型：type HandlerFunc func(ResponseWriter, *Request)。 这些只是说它是一个需要接受一个 ResponseWriter 和 Request 参数的函数，这对于 HTTP 服务器来说并不奇怪。 结果呢，这里并没有什么彩蛋，这也是如何在 Go 语言写一个 真实的 HTTP 服务器的方法。唯一的区别就是我们把它封装成一个易于测试的 httptest.NewServer，它会找一个可监听的端口，然后测试完你就可以关闭它了。 我们让两个服务器中慢的那一个短暂地 time.Sleep 一段时间，当我们请求时让它比另一个慢一些。然后两个服务器都会通过 w.WriteHeader(http.StatusOK) 返回一个 OK 给调用者。
    还是有些不明白

---
+++ select/v2/racer_test.go
@@ -0,0 +1,36 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		writer.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(
+		w http.ResponseWriter,
+		r *http.Request,
+	) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
